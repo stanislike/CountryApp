b8938cfc3f5926dec55f9dd99f4b7a485679f869
stanislike <s.joulah@outlook.fr>
2024-08-17 12:30:33 +0200

    Country App

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
-// 1 - Tester le lien de l'API dans le navigateur (https://restcountries.com/v3.1/all)
+// https://restcountries.com/v3.1/all
+const countriesContainer = document.querySelector(".countries-container");
+const btnSort = document.querySelectorAll(".btnSort");
+let country = [];
+let sortMethod = "maxToMin";
 
-// 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
+const fetchCountries = async () => {
+  await fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => (country = data));
 
-// 3 - Passer les données à une variable
+  displayCountries();
+  console.log(country);
+};
 
-// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
+const displayCountries = async () => {
+  countriesContainer.innerHTML = country
+    .filter((country) =>
+      country.translations.fra.common
+        .toLowerCase()
+        .includes(inputSearch.value.toLowerCase())
+    )
+    .sort((a, b) => {
+      if (sortMethod === "maxToMin") {
+        return b.population - a.population;
+      } else if (sortMethod === "minToMax") {
+        return a.population - b.population;
+      } else if (sortMethod === "alpha") {
+        return a.translations.fra.common.localeCompare(
+          b.translations.fra.common
+        );
+      }
+    })
+    .slice(0, inputRange.value)
+    .map((country) => {
+      return `
+        <div class="card">
+        <img src=${country.flags.svg} alt=${country.flags.alt} >
+        <h2>${country.translations.fra.common}</h2>
+        <h4>${country.capital}</h4>
+        <p>Population : ${country.population.toLocaleString()}</p>
+        
+        </div>
+        `;
+    })
+    .join("");
+};
 
-// 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-coutry.name.includes(inputSearch.value);
+window.addEventListener("load", fetchCountries);
+inputSearch.addEventListener("input", displayCountries);
 
-// 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
+inputRange.addEventListener("input", () => {
+  displayCountries();
+  rangeValue.textContent = inputRange.value;
+});
 
-// 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
+btnSort.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    sortMethod = e.target.id;
+    displayCountries();
+  });
+});
